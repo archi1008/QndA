d7fad8dc89ad3c1bf5ff4b2a1070f050b504d753
archi1008 <gargarchi941@gmail.com>
2023-04-16 23:17:18 +0530

    changes

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ const doc = {content: fileContent}
 const app = express();
 dotenv.config();
 app.use(cors());
-const allowedOrigins = ['http://localhost:3000', 'https://643c1506ccf92665547ee164--regal-sfogliatella-8e70d3.netlify.app/'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// const allowedOrigins = ['http://localhost:3000', 'https://643c1506ccf92665547ee164--regal-sfogliatella-8e70d3.netlify.app/'];
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 app.use(express.json());
 
     // mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology:true}).then((data)=>{
